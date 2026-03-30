f7f0dd1d7713cdbc66e08226eeea0bca0de5c30f
Abdallah Bouali <95148376+imbouali@users.noreply.github.com>
2026-03-30 18:10:02 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,15 @@ import { fileURLToPath, URL } from 'url'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ImBouali.github.io/',
+  
   resolve: {
+    
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  plugins: [react()],
-})
+  logLevel: 'error', // Suppress warnings, only show errors
+  plugins: [
+    react(),
+  ]
+});
