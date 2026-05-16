eefdbc1c903e07d854b8b1ce3d292f10f8cc8340
imbouali <abdallahbouali704@gmail.com>
2026-05-16 14:56:34 +0200

    Remplacer ancien projet par portfolio galaxy Cyber Abdallah
    Supprime : TypeScript/Vite/React, backend, web/, mp3, PDF, configs
    Mise a jour : workflow GitHub Pages pour site statique HTML/CSS/JS

--- web/src/main.tsx
+++
@@ -1,5 +0,0 @@
-import { createRoot } from "react-dom/client";
-import PortfolioApp from "./PortfolioApp";
-import "./index.css";
-
-createRoot(document.getElementById("root")!).render(<PortfolioApp />);
